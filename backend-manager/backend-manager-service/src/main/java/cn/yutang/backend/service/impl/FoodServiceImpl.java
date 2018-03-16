e54324032a091c8ef5e56891775e4de8d87c9bec
PetersFish <xifangshibai@126.com>
2018-03-16 13:10:44 +0800

    2018-3-16 第一次更新

--- backend-manager/backend-manager-service/src/main/java/cn/yutang/backend/service/impl/FoodServiceImpl.java
+++ backend-manager/backend-manager-service/src/main/java/cn/yutang/backend/service/impl/FoodServiceImpl.java
@@ -1,8 +1,29 @@
 package cn.yutang.backend.service.impl;
 
+import cn.yutang.backend.dao.FoodCustomMapper;
+import cn.yutang.backend.dao.FoodMapper;
+import cn.yutang.backend.pojo.po.Food;
 import cn.yutang.backend.service.IFoodService;
+import cn.yutang.commons.page.Page;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodServiceImpl implements IFoodService {
+
+	@Autowired
+	FoodCustomMapper foodCustomMapper;
+
+	@Override
+	public List<Food> listFoodByPage( Page page, Food food) {
+		return foodCustomMapper.listByPage(page,food);
+	}
+
+	@Override
+	public Integer countTotal(Food food) {
+		return foodCustomMapper.countTotal(food);
+	}
 }
