4580713cfa469308c4d42d2fed57b477263dac47
PetersFish <xifangshibai@126.com>
2018-03-16 15:32:57 +0800

    2018-3-16 第二次更新

---
+++ commons/src/main/java/cn/yutang/commons/page/Page.java
@@ -0,0 +1,34 @@
+package cn.yutang.commons.page;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@Scope(value = "prototype")
+public class Page implements Serializable{
+	private Integer page = 1;
+	private Integer limit;
+	private Integer offset;
+
+	public Integer getOffset() {
+		return (page-1)*limit;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public Integer getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+}
